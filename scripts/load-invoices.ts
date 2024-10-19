79efe9083b9c3e176ef8818ce9d26edda44e7ab0
alepontes <alexandrelopespontes@gmail.com>
2024-10-19 13:32:48 -0300

    Adicionar extrator de informações de boletos

---
+++ scripts/load-invoices.ts
@@ -0,0 +1,60 @@
+import {readdirSync, readFileSync} from 'fs';
+import {readFile as _readFile, renameSync, writeFileSync} from "node:fs";
+import {getData} from "../src/lib";
+const pdf = require('pdf-parse');
+
+// path das faturas
+const invoicesPath = '/home/ale/Archive/Develop/test-lumi/Develop/boletos-api/playground/faturas';
+
+// carregas itens do diretório
+const files = readdirSync(invoicesPath);
+
+/**
+ * Converte um PDF em Texto
+ */
+const pdftoText = async (_buffer: Buffer): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        pdf(_buffer).then(({text}: {text: string }) => {
+            resolve(text);
+        });
+    });
+}
+
+(async () => {
+
+    // lê os arquivos simultaneamente
+    const promises = files
+        .filter(file => file.endsWith('.pdf'))
+        .map(file => new Promise(async (resolve, reject) => {
+
+            // path do arquivo
+            const path = `${invoicesPath}/${file}`;
+
+            // lê o arquivo
+            const _buffer = readFileSync(path);
+
+            // converte PDF em Texto
+            const text = await pdftoText(_buffer);
+
+            // recupera dados do PDF
+            const data = await getData(text);
+
+            // move para processados
+            renameSync(path, `${invoicesPath}/processed/${file}`);
+
+            // // persiste resultado
+            // writeFileSync(path.replace('.pdf', '.json'), JSON.stringify(data, null, 2), { encoding: 'utf-8' });
+
+            resolve(data);
+        }));
+
+    // resolve promisses
+    const list = await Promise.all(promises);
+
+    // persiste resultado
+    writeFileSync(`${invoicesPath}/out.json`, JSON.stringify(list, null, 2), { encoding: 'utf-8' });
+
+    console.log(list);
+
+})()
+
---
+++ src/lib.ts
@@ -0,0 +1,129 @@
+// @ts-ignore
+import pdf from 'pdf-parse';
+import fs from 'fs';
+
+
+/**
+ * Converte um PDF em Texto
+ */
+const pdftoText = async (_buffer: any) => {
+    return new Promise((resolve, reject) => {
+        pdf(_buffer).then(({text}: { text: string }) => {
+            resolve(text);
+        });
+    });
+}
+
+/**
+ *
+ */
+const extractPersonalInformation = (text: string) => {
+    const regex = new RegExp(/\s+Nº DO CLIENTE\s+Nº DA INSTALAÇÃO\s+(\d+)\s+(\d+)/, 'g');
+    const [, clientId, meterId] = regex.exec(text) || [];
+
+    return {
+        clientId,
+        meterId,
+    }
+}
+
+/**
+ *
+ * @param text
+ * @returns {Promise<void>}
+ */
+const extractGeneralInformation = (text: string) => {
+    const regex = new RegExp(/\s+Referente a\s+Vencimento\s+Valor a pagar \(R\$\)\s+([A-Z]{3}\/\d{4})\s+(\d{2}\/\d{2}\/\d{4})\s+(\d+(,\d+)?)/, 'g');
+    const [, date, due, payment] = regex.exec(text) || [];
+
+    return  {
+        date,
+        due,
+        payment,
+    }
+
+}
+
+/**
+ *
+ * @param text
+ * @returns {{priceUnit: string, quantity: string, tax: string, value: string}}
+ */
+const extractElectricalEnergyInformation = (text: string) => {
+    const regex = new RegExp(/Energia ElétricakWh\s+(\d+)\s+(\d+(?:,\d+)?)\s+(-?\d+(?:,\d+)?)\s+(\d+(?:,\d+)?)/, 'g');
+    const [, quantity, priceUnit, value, tax] = regex.exec(text) || [];
+
+    return {
+        quantity,
+        priceUnit,
+        value,
+        tax,
+    }
+
+}
+
+const extractSceeInformation = (text: string) => {
+    const regex = new RegExp(/Energia SCEE s\/ ICMSkWh\s+(\d+)\s+(\d+(?:,\d+)?)\s+(-?\d+(?:,\d+)?)\s+(\d+(?:,\d+)?)/, 'g');
+    const [, quantity, priceUnit, value, tax] = regex.exec(text) || [];
+
+    return {
+        quantity,
+        priceUnit,
+        value,
+        tax,
+    }
+}
+
+const extractCompensatedEnergyInformation = (text: string) => {
+    const regex = new RegExp(/Energia compensada GD IkWh\s+(\d+)\s+(\d+(?:,\d+)?)\s+(-?\d+(?:,\d+)?)\s+(\d+(?:,\d+)?)/, 'g');
+    const [, quantity, priceUnit, value, tax] = regex.exec(text) || [];
+
+    return {
+        quantity,
+        priceUnit,
+        value,
+        tax,
+    }
+
+}
+
+const extractPublicEnergyInformation = (text: string) => {
+    const regex = new RegExp(/Contrib Ilum Publica Municipal\s+(-?\d+(?:,\d+)?)/);
+    const [, value] = regex.exec(text) || [];
+
+    return {
+        value,
+    }
+
+}
+
+const extractTotalValue = (text: string) => {
+    const regex = new RegExp(/TOTAL\s+(-?\d+(?:,\d+)?)/);
+    const [, value] = regex.exec(text) || [];
+
+    return {
+        value,
+    }
+}
+
+export const getData = async (text: string) => {
+
+    const personal = extractPersonalInformation(text);
+    const general = extractGeneralInformation(text);
+    const electricalEnergy = extractElectricalEnergyInformation(text);
+    const scee = extractSceeInformation(text);
+    const compensatedEnergy = extractCompensatedEnergyInformation(text);
+    const publicEnergy = extractPublicEnergyInformation(text);
+    const total = extractTotalValue(text);
+
+    return {
+        personal,
+        general,
+        electricalEnergy,
+        scee,
+        compensatedEnergy,
+        publicEnergy,
+        total,
+    }
+
+}
