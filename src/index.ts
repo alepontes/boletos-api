becad119565012aefec84697f74b6b8951d5fa04
alepontes <alexandrelopespontes@gmail.com>
2024-10-19 21:30:54 -0300

    Ajustar cadastro e listagem de faturas

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,60 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client'
 
 const server = fastify();
 server.register(cors, {});
 
-server.get('/hello', async (request, reply) => {
-    return 'world';
-})
+const prisma = new PrismaClient();
 
-server.get('/invoices', async () => {
+const dto = (invoice: any) => {
+    return {
+        personal: {
+            clientId: invoice.clientId,
+            name: invoice.name,
+            meterId: invoice.meterId
+        },
+        general: {
+            date: invoice.date,
+            due: invoice.due,
+            payment: invoice.payment,
+            distributor: invoice.distributor,
+        },
+        electricalEnergy: {
+            quantity: invoice.electricalQuantity,
+            priceUnit: invoice.electricalPriceUnit,
+            value: invoice.electricalValue,
+            tax: invoice.electricalTax
+        },
+        scee: {
+            quantity: invoice.sceeQuantity,
+            priceUnit: invoice.sceePriceUnit,
+            value: invoice.sceeValue,
+            tax: invoice.sceeTax
+        },
+        compensatedEnergy: {
+            quantity: invoice.compensatedQuantity,
+            priceUnit: invoice.compensatedPriceUnit,
+            value: invoice.compensatedValue,
+            tax: invoice.compensatedTax
+        },
+        publicEnergy: {
+            value: invoice.publicEnergyValue
+        },
+        total: {
+            value: invoice.totalValue
+        }
+    }
+}
 
-    // mock
-    return [
-        {
-            nomeUC: "CASA DONA COMERCIO VAREJISTA E SOLUÇÕES",
-            numeroUC: "3002863513",
-            distribuidora: "CEMIG",
-            consumidor: "CASA DONA COMERCIO VAR...",
-        },
-        {
-            nomeUC: "Walter Boaventura da Silva",
-            numeroUC: "3003336712",
-            distribuidora: "CEMIG",
-            consumidor: "Walter Boaventura da Silva",
-        },
-    ]
+server.get('/invoices', async (request, reply) => {
+
+    const invoices = await prisma.invoice.findMany();
+
+    console.log('invoices');
+    console.log(invoices);
+
+    reply.send(invoices.map(invoice => dto(invoice)));
 
 });
 
@@ -35,6 +65,37 @@ server.get('/filters', async () => {
         years: [2018, 2019, 2020, 2021, 2022, 2023, 2024]
     }
 
+});
+
+server.post('/invoices', async (request, reply) => {
+
+    const data = request.body as any;
+
+    const invoice = await prisma.invoice.create({ data: {
+            clientId: data.personal.clientId,
+            name: data.personal.name,
+            meterId: data.personal.meterId,
+            date: data.general.date,
+            due: data.general.due,
+            payment: data.general.payment,
+            distributor: data.general.distributor,
+            electricalQuantity: data.electricalEnergy.quantity,
+            electricalPriceUnit: data.electricalEnergy.priceUnit,
+            electricalValue: data.electricalEnergy.value,
+            electricalTax: data.electricalEnergy.tax,
+            sceeQuantity: data.scee.quantity,
+            sceePriceUnit: data.scee.priceUnit,
+            sceeValue: data.scee.value,
+            sceeTax: data.scee.tax,
+            compensatedQuantity: data.compensatedEnergy.quantity,
+            compensatedPriceUnit:data.compensatedEnergy.priceUnit,
+            compensatedValue:data.compensatedEnergy.value,
+            compensatedTax: data.compensatedEnergy.tax,
+            publicEnergyValue: data.publicEnergy.value,
+            totalValue: data.total.value,
+        } });
+
+    return dto(invoice);
 });
 
 server.listen({ port: 8080 }, (err, address) => {
